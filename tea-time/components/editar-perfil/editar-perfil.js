60dabd7f7975c3ec60ffa855544ae2e36e15b0f7
bii-mm <61274369+bii-mm@users.noreply.github.com>
2021-05-16 18:20:44 -0300

    alterei o responsivo do perfil

--- tea-time/components/editar-perfil/editar-perfil.js
+++ tea-time/components/editar-perfil/editar-perfil.js
@@ -4,39 +4,39 @@ function EditPerfil(){
     return (
         <div>
             <div className = {classes.editar}>
-            <h1>Editar Perfil do Usuário:</h1>
+            <h1>Editar Perfil do Usuário</h1>
             </div>
             <div className = {classes.style}>
                 <div className={classes.control}>
-                    <h1>Alterar dados:</h1>
+                    <h1>Alterar dados</h1>
                 </div>
                 <form className={classes.form}>
                 <div className={classes.control}>
-                    <label htmlFor="nome"> Nome Completo: </label>
+                    <label htmlFor="nome"> Nome Completo </label>
                     <input type="text" id="nome" required />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor="dataN"> Data de Nascimento: </label>
+                    <label htmlFor="dataN"> Data de Nascimento </label>
                     <input type="date" id="dataN" required />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor="estado"> Estado:  </label>
+                    <label htmlFor="estado"> Estado  </label>
                     <input type="text" id="estado" required />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor="cidade"> Cidade: </label>
+                    <label htmlFor="cidade"> Cidade </label>
                     <input type="text" id="cidade" required />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor="user"> Usuário: </label>
+                    <label htmlFor="user"> Usuário </label>
                     <input type="text" id="user" required />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor="email"> Alterar E-mail: </label>
+                    <label htmlFor="email"> Alterar E-mail </label>
                     <input type="email" id="email" required />
                 </div>
                 <div className={classes.control}>
-                    <label htmlFor="password"> Senha: </label>
+                    <label htmlFor="password"> Senha </label>
                     <input type="password" id="password" required />
                 </div>
                 <br/>
@@ -48,21 +48,21 @@ function EditPerfil(){
         </div>
         <div className = {classes.style}>
                 <div className={classes.control}>
-                    <h1>Alterar senha:</h1>
+                    <h1>Alterar senha</h1>
                 </div>
                 <form className={classes.form}>
                 <div className={classes.control}>
-                    <label htmlFor="password">Senha Antiga: </label>
+                    <label htmlFor="password">Senha Antiga </label>
                     <input type="password" id="password" required />
                 </div>
                 <br/>
                 <div className={classes.control}>
-                    <label htmlFor="password"> Senha Nova: </label>
+                    <label htmlFor="password"> Senha Nova </label>
                     <input type="password" id="password" required />
                 </div>
                 <br/>
                 <div className={classes.control}>
-                    <label htmlFor="password"> Confirme Senha Nova: </label>
+                    <label htmlFor="password"> Confirme Senha Nova </label>
                     <input type="password" id="password" required />
                 </div>
                 <br/>
--- tea-time/components/perfil/perfil-usuario.js
+++ tea-time/components/perfil/perfil-usuario.js
@@ -1,7 +1,6 @@
 import classes from "./perfil-usuario.module.css";
 import Link from "next/link";
 
-
 function UsuarioPage(){
     return (
     <div>
@@ -12,101 +11,103 @@ function UsuarioPage(){
         <h1>MEU PERFIL</h1>
             <h2>Suas Avaliações:</h2>
         </div>
-        <div >
-            <div className = {classes.amigos}>
+        
+        <div className = {classes.megadiv}>
+            <div className = {classes.esquerda}>
+                <div className = {classes.filmes}>
                 <div className = {classes.box1}>
-                    <h1>AMIGOS</h1>
-                </div>
-                <div className = {classes.box2}>
-                <ul>
-                    <li># amigo 1</li>
-                    <br/>
-                    <li># amigo 2</li>
-                    <br/>
-                    <li># amigo 3</li>
-                    <br/>
-                    <li># amigo 4</li>
-                    <br/>
-                    <li># amigo 5</li>
-                    <br/>
-                    <li># amigo 6</li>
-                    <br/>
-                    <li># amigo 7</li>
-                    <br/>
-                    <li># amigo 8</li>
-                    <br/>
-                    <li># amigo 9</li>
-                    <br/>
-                    <li># amigo 10</li>
-                    <br/>
-                </ul>
-                </div>
-            </div>
-        </div>
-        <div className = {classes.perfil}>
-            <div className = {classes.box1}>
-            <h1>FILMES</h1>
-            </div>
-        <div className = {classes.box2}>
-            <br/>
-        <p>Você não possui nenhuma avaliação ainda :( </p>
-        </div>
-        <br/>
-        </div>
-        <br/>
-        <div className = {classes.perfil}>
-            <div className = {classes.box1}>
-            <h1>LIVROS</h1>
-            </div>
-            <div className = {classes.box2}>
-            <br/>
-            <p>Você não possui nenhuma avaliação ainda :( </p>
-            <br/>
-            </div>
-        </div>
-        <br/>
-        <div className = {classes.perfil}>
-            <div className = {classes.box1}>
-            <h1>SÉRIES</h1>
-            </div>
-            <div className = {classes.box2}>
-            <br/>
-            <p>Você não possui nenhuma avaliação ainda :( </p>
-            <br/>
-            </div>
-        </div>
-        <div className = {classes.sugestao}>
-                    <div className = {classes.box1}>
-                        <h1>SUGESTÃO DE AMIGOS</h1>
+                    <h1>FILMES</h1>
                     </div>
                     <div className = {classes.box2}>
-                    <ul>
-                            <li># Membro 1</li>
-                            <br/>
-                            <li># Membro 2</li>
-                            <br/>
-                            <li># Membro 3</li>
-                            <br/>
-                            <li># Membro 4</li>
-                            <br/>
-                            <li># Membro 5</li>
-                            <br/>
-                            <li># Membro 6</li>
-                            <br/>
-                            <li># Membro 7</li>
-                            <br/>
-                            <li># Membro 8</li>
-                            <br/>
-                            <li># Membro 9</li>
-                        </ul>
+                    <br/>
+                    <p>Você não possui nenhuma avaliação ainda :( </p>
+                    <br/>
                     </div>
-        </div>
-        <div className = {classes.perfil}>
-            <div className = {classes.box1}>
-                <h1>RECOMENDAÇÕES</h1>
+                </div>
+
+                <div className = {classes.livros}>
+                <div className = {classes.box1}>
+                    <h1>LIVROS</h1>
+                    </div>
+                    <div className = {classes.box2}>
+                    <br/>
+                    <p>Você não possui nenhuma avaliação ainda :( </p>
+                    <br/>
+                    </div>
+                </div>
+
+                <div className = {classes.series}>
+                    <div className = {classes.box1}>
+                    <h1>SÉRIES</h1>
+                    </div>
+                    <div className = {classes.box2}>
+                    <br/>
+                    <p>Você não possui nenhuma avaliação ainda :( </p>
+                    <br/>
+                    </div>
+                    
+                </div>
+
+                <div className = {classes.recomendacoes}>
+                        <div className = {classes.box1}>
+                        <h1>RECOMENDAÇÕES</h1>
+                        </div>
+                        <br/>
+                        <p>Faça uma avaliação para receber recomendações :)</p>
+                        <br/>
+                </div>
             </div>
-            <div className = {classes.box2}>
-                <p>Faça uma avaliação para receber recomendações :)</p>
+
+            <div className = {classes.direita}>
+                <div className = {classes.amigos} className = {classes.box1}>
+                        <h1>AMIGOS</h1>
+                        <br/>
+                    <ul>
+                        <li># amigo 1</li>
+                        <br/>
+                        <li># amigo 2</li>
+                        <br/>
+                        <li># amigo 3</li>
+                        <br/>
+                        <li># amigo 4</li>
+                        <br/>
+                        <li># amigo 5</li>
+                        <br/>
+                        <li># amigo 6</li>
+                        <br/>
+                        <li># amigo 7</li>
+                        <br/>
+                        <li># amigo 8</li>
+                        <br/>
+                        <li># amigo 9</li>
+                        <br/>
+                        <li># amigo 10</li>
+                        <br/>
+                    </ul>
+                </div>
+                <div className = {classes.sugestoes} className = {classes.box1}>
+                            <h1>SUGESTÃO DE AMIGOS</h1>
+                        <ul>
+                                <li># Membro 1</li>
+                                <br/>
+                                <li># Membro 2</li>
+                                <br/>
+                                <li># Membro 3</li>
+                                <br/>
+                                <li># Membro 4</li>
+                                <br/>
+                                <li># Membro 5</li>
+                                <br/>
+                                <li># Membro 6</li>
+                                <br/>
+                                <li># Membro 7</li>
+                                <br/>
+                                <li># Membro 8</li>
+                                <br/>
+                                <li># Membro 9</li>
+                                <br/>
+                            </ul>
+                </div>
             </div>
         </div>
     </div>
